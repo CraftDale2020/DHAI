af7e2cef582cd018af7d6ce3397155017edd3b79
CraftDale <61933509+CraftDale2020@users.noreply.github.com>
2023-01-15 02:10:43 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('https://dhai.onrender.com/', {
+    const response = await fetch('https://dhai2.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
